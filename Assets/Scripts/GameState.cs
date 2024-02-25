af9b1cd6428909972332028c7fa3c82d39170afd
sudzoof <60050712+sudzoof@users.noreply.github.com>
2024-02-24 19:22:21 -0500

    Add enemy AI
    Logic still needs to be done for attacking/recognizing ranges

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using TreeEditor;
 
 public class GameState
 {
@@ -8,6 +9,9 @@ public class GameState
     private readonly Tile[][] tiles;
     public Tile[][] Tiles => tiles;
 
+    private readonly float[,] costMap;
+
+    public float[,] CostMap => costMap;
 
     private readonly List<string> turnOrder = new List<string>();
     public IList<string> TurnOrder => turnOrder;
@@ -26,6 +30,26 @@ public class GameState
     public GameState(Tile[][] map)
     {
         tiles = map;
+        costMap = MakeCostMap();
+    }
+
+    private float[,] MakeCostMap()
+    {
+        // TODO check that the dimensions are correct
+        float[,] costMap = new float[tiles.Length, tiles[0].Length];
+        for (int i = 0; i < costMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < costMap.GetLength(1); j++)
+            {
+                switch (tiles[i][j])
+                {
+                    case Tile.Basic:
+                        costMap[i, j] = 1;
+                        break;
+                }
+            }
+        }
+        return costMap;
     }
 
 }
--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -1,26 +1,76 @@
+using AStar;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
 
 public class TurnManager
 {
+
+    private List<string> turnOrder;
+    private int turnIndex;
+
     public void StartRound(GameState gameState)
     {
         // get all units
         // put them into the turn order
+        turnOrder = gameState.CurrentUnits.Keys.OrderBy(_ => UnityEngine.Random.Range(0f, 1f)).ToList();
+        turnIndex = 0;
     }
 
     public void TakeTurn(GameState gameState)
     {
-
+        string nextUnit = turnOrder[turnIndex];
+        switch (gameState.CurrentUnits[nextUnit].Type)
+        {
+            case ActorType.Player:
+                DoPlayerTurn(nextUnit, gameState);
+                break;
+            case ActorType.Ally:
+            case ActorType.AI:
+                DoAITurn(nextUnit, gameState);
+                break;
+        }
+        turnIndex++;
     }
 
-    private void DoPlayerTurn(string charId)
+    private void DoPlayerTurn(string charId, GameState gameState)
     {
 
     }
 
-    private void DoAITurn(string charId)
+    private void DoAITurn(string charId, GameState gameState)
     {
-
+        (int, int)[] path = GetClosestPath(charId, gameState);
+        Actor curActor = gameState.CurrentUnits[charId];
+        // TODO recognize movement penalties
+        Vector2Int targetPos = new Vector2Int(path[curActor.Movement].Item1, path[curActor.Movement].Item2);
+        curActor.Move(targetPos);
     }
+
+    private (int, int)[] GetClosestPath(string charId, GameState gameState)
+    {
+        IDictionary<string, Actor> curUnits = gameState.CurrentUnits;
+        Actor curActor = curUnits[charId];
+        (int, int)[] shortestPath = null;
+        foreach ((string key, Actor value) in curUnits)
+        {
+            if (!charId.Equals(key))
+            {
+                (int, int)[] path = AStarPathfinding.GeneratePathSync(curActor.Position.x, curActor.Position.y,
+                    value.Position.x, value.Position.y, gameState.CostMap, true, false);
+                // TODO recognize movement penalties for path cost
+                if (shortestPath == null || shortestPath.Length > path.Length)
+                {
+                    shortestPath = path;
+                }
+            }
+
+        }
+        return shortestPath;
+    }
+
 }
