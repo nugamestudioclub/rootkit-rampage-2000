1b4d131cfe1d6559442c3c9835f18e5250568206
Sebastian Kobori <sjkobori@gmail.com>
2024-02-25 03:51:04 -0500

    Updated game start to bootstrap (needs map config still)

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -1,24 +1,37 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameStart : MonoBehaviour
 {
+    //TODO: Add map config 12x8 tile grid
+    
+    [field: SerializeField]
+    public List<ActorConfig> ActorConfigs { get; private set; }
 
+    [field: SerializeField]
+    public List<AbilityConfig> AbilityConfigs { get; private set; }
 
-    //public Start(MapConfig map, IList<ActorConfig> actorConfigs, )
-    //{
+    [field: SerializeField]
+    public UIManager UIManager { get; private set; }
 
-    //}
     public GameManager Manager { get; private set; }
     void Awake()
     {
-        Manager = new GameManager();
+        IEnumerable<Actor> actors = ActorConfigs.Select((ac) => ac.Generate());
+        IEnumerable<Ability> abilities = AbilityConfigs.Select((ac) => ac.Generate());
+        //ADD MAP to game state
+        Manager = new GameManager(
+            new GameState(null),
+            new TurnManager(),
+            UIManager,
+            actors.ToList(),
+            abilities.ToList()
+        );
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Manager.Tick();
     }
 }
