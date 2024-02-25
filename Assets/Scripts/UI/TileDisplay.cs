ef173fd02cf43a5419136a7e433e682705f6b8c2
Sebastian Kobori <sjkobori@gmail.com>
2024-02-25 16:13:06 -0500

    WIP tile display

---
+++ Assets/Scripts/UI/TileDisplay.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TileDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private Grid grid;
+
+
+    // Update is called once per frame
+    void UpdateTiles(Tile[,] tiles)
+    {
+        
+    }
+}
